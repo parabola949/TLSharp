344fd444204f7c3dc650f93c49b02277d9e7c3d0
parabola949 <parabola949@gmail.com>
2016-12-20 08:55:57 -0600

    sync with upstream repo

--- TeleSharp.TL/TL/Messages/TLRequestGetFullChat.cs
+++ TeleSharp.TL/TL/Messages/TLRequestGetFullChat.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using TeleSharp.TL;
 namespace TeleSharp.TL.Messages
 {
-	[TLObject(998448230)]
+    [TLObject(998448230)]
     public class TLRequestGetFullChat : TLMethod
     {
         public override int Constructor
@@ -18,31 +18,30 @@ namespace TeleSharp.TL.Messages
             }
         }
 
-                public int chat_id {get;set;}
-        public Messages.TLChatFull Response{ get; set;}
+        public int chat_id { get; set; }
+        public Messages.TLChatFull Response { get; set; }
 
 
-		public void ComputeFlags()
-		{
-			
-		}
+        public void ComputeFlags()
+        {
+
+        }
 
         public override void DeserializeBody(BinaryReader br)
         {
             chat_id = br.ReadInt32();
-
         }
 
         public override void SerializeBody(BinaryWriter bw)
         {
-			bw.Write(Constructor);
+            bw.Write(Constructor);
             bw.Write(chat_id);
 
         }
-		public override void deserializeResponse(BinaryReader br)
-		{
-			Response = (Messages.TLChatFull)ObjectUtils.DeserializeObject(br);
+        public override void deserializeResponse(BinaryReader br)
+        {
+            Response = (Messages.TLChatFull)ObjectUtils.DeserializeObject(br);
 
-		}
+        }
     }
 }
